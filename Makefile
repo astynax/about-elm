21217970602867cdf4ae72028841d51ff95b09fd
Aleksey Pirogov <astynax84@gmail.com>
2016-02-28 16:32:08 +0300

    add slides (uncompleted for now)

---
+++ Makefile
@@ -0,0 +1,7 @@
+MAKE_SLIDES = elm-make Slides.elm --output dist/slides.js
+
+slides:
+	$(MAKE_SLIDES)
+
+watch:
+	hobbes Slides.elm | xargs -I {} $(MAKE_SLIDES)
---
+++ Slides.elm
@@ -0,0 +1,236 @@
+module Slides where
+
+import Effects exposing (Effects)
+import Keyboard
+import Signal exposing (Address, Signal)
+
+import Html exposing (Html, text, a, p, pre, code, div, span, img, br, ul, li)
+import Html.Attributes exposing (class, href, src)
+import Html.Events exposing (onClick)
+
+import StartApp exposing (App, start)
+
+main : Signal Html
+main = app.html
+
+app : App Model
+app =
+  StartApp.start
+  { init = (init slides, Effects.none)
+  , view = view
+  , update = update
+  , inputs = [ leftRight ]
+  }
+
+type Action = Forward | Backward
+
+type alias Slide = Address (Maybe.Maybe Action) -> Html.Html
+type alias UnindexedSlide = (Int, Int) -> Slide
+type alias Model = (List Slide, List Slide)
+
+slides : List UnindexedSlide
+slides =
+  [ title <| slide ""
+      [ text "Разработка", nl
+      , text "интерактивных", nl
+      , text "Web-приложений", nl
+      , text "на языке Elm", nl, nl
+      , img [ src "static/elm.png" ] []
+      ]
+  , slide "Elm?"
+      [ text "Elm, это"
+      , ul_
+          [ text "Функциональный язык"
+          , text "Сильная статическая типизация"
+          , text "Алгебраические Типы Данных"
+          , text "Компиляция в JavaScript"
+          , text "Нацеленность на построение UI"
+          , text "Контроль над side-эффектами"
+          , text "FRP, Time-travel Debugging, ..."
+          ]
+      ]
+  , slide "Установка"
+      [ source
+          "shell"
+          "$ npm install -g elm"
+      , ul_ <| List.map
+          code_
+          [ "elm-make"
+          , "elm-package"
+          , "elm-reactor"
+          , "elm-repl"
+          ]
+      ]
+  , slide "Синтаксис"
+      [ source
+          "elm"
+          "-- Алгебраические Типы Данных
+
+type Color = Red | Green | Blue
+
+type Point = Point Int Int
+
+type Shape
+  = Circle   Point Int
+  | Triangle Point Point Point
+  | Rect     Point Point"
+      ]
+  , slide "Синтаксис"
+      [ source
+          "elm"
+          "-- Записи (Records)
+
+type User =
+  User { name : String
+       , age : Int
+       }
+
+user = User { name = \"Moe\", age = 42 }
+
+userAge = user.age
+
+newUser = { user | age = user.age + 1 }"
+      ]
+  , slide "Синтаксис"
+      [ source
+          "elm"
+          "-- Функции
+
+add : Int -> Int -> Int
+add x y = x + y
+
+wrap
+  : String -> String -> Int -> String
+wrap s1 s2 x =
+  String.append s1
+  <| String.append (toString x) s2
+
+inBrackets : Int -> String
+inBrackets = wrap \"[\" \"]\""
+      ]
+  , slide "Базис"
+      [ ul_
+          [ text "Element"
+          , text "Signal"
+          ]
+      ]
+  , demo
+  , slide "Model-View-Update"
+      [ ul_
+          [ code_ "evancz/start-app"
+          , code_ "evancz/elm-html"
+          , text "SimpleApp, Html"
+          ]
+      ]
+  , demo
+  , slide "Effects"
+      [ ul_
+          [ code_ "evancz/elm-effects"
+          , text "Effects"
+          ]
+      ]
+  , demo
+  , slide "HTTP"
+      [ ul_
+          [ code_ "evancz/elm-http"
+          , text "Http, Task"
+          ]
+      ]
+  , demo
+  , title <| slide "The End" <:: text "Вопросы?"
+  ]
+
+init : List UnindexedSlide -> Model
+init slides =
+  let
+    count = List.length slides
+  in
+    ( slides |> List.indexedMap (\idx s -> s (idx + 1, count))
+    , []
+    )
+
+view : Address (Maybe Action) -> Model -> Html.Html
+view address model =
+  case model of
+    ((s :: _), _) -> s address
+    _ -> text "Oops!"
+
+
+update : Maybe Action -> Model -> (Model, Effects a)
+update action model =
+  ( case (action, model) of
+      (Just Forward, ((s :: s' :: rest), prev)) ->
+        (s' :: rest, s :: prev)
+
+      (Just Backward, (rest, (s :: prev))) ->
+        (s :: rest, prev)
+
+      _ -> model
+  , Effects.none
+  )
+
+-- input
+
+leftRight : Signal (Maybe Action)
+leftRight =
+  Keyboard.arrows
+    |> Signal.map (\ev ->
+                     case (ev.x, ev.y) of
+                       (-1, 0) -> Just Backward
+                       (1,  0) -> Just Forward
+                       _       -> Nothing
+                  )
+
+-- view helpers
+
+slide : String -> List Html -> UnindexedSlide
+slide header content (idx, count) address =
+  let
+    navButton active label action =
+      a ( List.append
+            [ href "#" ]
+            (if active
+             then [ class "nav-btn"
+                  , onClick address action ]
+             else [ class "nav-btn disabled" ]
+            )
+        ) [ text label ]
+    navBar =
+      div [ class "nav-bar" ]
+            [ navButton (idx > 1) "<<" (Just Backward)
+            , div [ class "pages" ]
+                [ text <| toString idx
+                , text "/"
+                , text <| toString count
+                ]
+            , navButton (idx < count) ">>" (Just Forward)
+            ]
+  in
+    div [ class "slide" ]
+          [ div [ class "header" ] <:: text header
+          , div [ class "frame" ] [ div [ class "content" ] content ]
+          , navBar
+          ]
+
+title : UnindexedSlide -> UnindexedSlide
+title s i a = div [ class "title" ] [ s i a ]
+
+demo : UnindexedSlide
+demo = title <| slide "" <:: text "demo"
+
+nl : Html
+nl = br [] []
+
+ul_ : List Html -> Html
+ul_ = ul [] << List.map ((<::) (li []))
+
+code_ : String -> Html
+code_ = (<::) (code []) << text
+
+source : String -> String -> Html
+source hl s = pre [ class "src" ] <:: code [ class hl ] <:: text s
+
+
+infixr 9 <::
+(<::) : (List a -> b) -> a -> b
+(<::) f x = f [ x ]
